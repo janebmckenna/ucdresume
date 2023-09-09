2e373abcd9383517546a994538b30940bc1bc498
janebmckenna <>
2023-09-09 10:46:53 +0000

    fix: fix error with map js set up

--- assets/js/maps.js
+++ assets/js/maps.js
@@ -1,10 +1,10 @@
 // 
 function initMap() {
 var map = new google.maps.Map(document.getElementById("map"), {
-    zoom: 3,
+    zoom: 8,
     center: {
-        lat: 46.619261,
-        lng: -33.134766
+        lat: 40.7128,
+        lng: -74.0060
     }
 });
 
@@ -25,3 +25,4 @@ var markers = locations.map(function (location, i) {
 
 var markerCluster = new MarkerClusterer(map, markers, { imagePath: 'https://developers.google.com/maps/documentation/javascript/examples/markerclusterer/m' });
 }
+
